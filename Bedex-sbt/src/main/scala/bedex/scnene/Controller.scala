3b3a72cb31ce525f27de3c89abe8f5b4262bef0e
Dudu <edu.chaos@gmail.com>
2013-09-05 17:45:50 -0300

    Test XML loading with controller handling

---
+++ Bedex-sbt/src/main/scala/bedex/scnene/Controller.scala
@@ -0,0 +1,44 @@
+package bedex.scnene
+
+import java.net.URL
+import java.util
+import javafx.scene.{ layout => jfxsl }
+import javafx.{ event => jfxe }
+import javafx.{ fxml => jfxf }
+import javafx.scene.{ control => ctrl }
+import scalafx.scene.layout.AnchorPane
+import scalafx.scene.control.Button
+import scalafx.scene.control.Label
+
+class Controller extends jfxf.Initializable {
+
+  @jfxf.FXML
+  private var myButton: ctrl.Button = _
+  private var button: Button = _
+
+  @jfxf.FXML
+  private var myLabel: ctrl.Label = _
+  private var label: Label = _
+
+  @jfxf.FXML
+  private var myLabelCount: ctrl.Label = _
+  private var labelCount: Label = _
+
+  private var count = 0
+
+  @jfxf.FXML
+  private def onMouseAction(event: jfxe.ActionEvent) {
+    count = count + 1
+    val msg = s"Hi there: ${new java.util.Date}"
+
+    println(msg)
+    label.text = msg
+    labelCount.text = s"${count} time${if (count > 1) "s" else ""} clicked!"
+  }
+
+  def initialize(url: URL, rb: util.ResourceBundle) {
+    button = new Button(myButton)
+    label = new Label(myLabel)
+    labelCount = new Label(myLabelCount)
+  }
+}
---
+++ Bedex-sbt/src/main/scala/bedex/scnene/Main.scala
@@ -0,0 +1,29 @@
+package bedex.scnene
+
+import java.io.IOException
+import javafx.{fxml => jfxf}
+import javafx.{scene => jfxs}
+import scalafx.Includes._
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.Scene
+
+object Main extends JFXApp {
+
+  val resource = getClass.getResource("Sample.fxml")
+  if (resource == null) {
+    throw new IOException("Cannot load resource: Sample.fxml")
+  }
+
+  // NOTE: ScalaFX doe not yet provide a wrapper fro FXMLLoader (2012.11.12)
+  // We load here FXML content using JavaFX directly.
+  // It is important to provide type for the element loaded,
+  // though it can be a generic, here use `javafx.scene.parent`.
+  val root: jfxs.Parent = jfxf.FXMLLoader.load(resource)
+
+  stage = new PrimaryStage() {
+    title = "FXML Sample Demo"
+    scene = new Scene(root)
+  }
+
+}
