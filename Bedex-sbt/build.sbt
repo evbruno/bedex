0845be2e9927e9de93fb71ecf2d724afcf464737
Dudu <edu.chaos@gmail.com>
2013-10-09 14:44:34 -0300

    Adding pre-notif UI

--- Bedex-sbt/build.sbt
+++ Bedex-sbt/build.sbt
@@ -2,7 +2,8 @@ import AssemblyKeys._ // put this at the top of the file
 
 assemblySettings
 
-mainClass in assembly := Some("bedex.BedexApp")
+// mainClass in assembly := Some("bedex.BedexApp")
+mainClass in assembly := Some("bedex.gui.BedexMainApp")
 
 name := "bedex"
 
--- Bedex-sbt/src/main/scala/bedex/gui/MainController.scala
+++ Bedex-sbt/src/main/scala/bedex/gui/MainController.scala
@@ -11,34 +11,37 @@ class MainController extends Initializable {
 
   @FXML
   private var mainContentTabPane: TabPane = _
-  
+
   @FXML
   private var holidayTab: Tab = _
 
   @FXML
   private var vacationTab: Tab = _
-  
+
   @FXML
   private var missAppointmentTab: Tab = _
-  
+
+  @FXML
+  private var preNotificationTab: Tab = _
+
   @FXML
   private var statusBarBox: HBox = _
-  
 
   def initialize(url: URL, rb: java.util.ResourceBundle) {
+    missAppointmentTab.setContent(new MainView().root)
     holidayTab.setContent(tabContent("Holiday"))
     vacationTab.setContent(tabContent("Vacation"))
-    missAppointmentTab.setContent(new MainView().root)
-    
+    preNotificationTab.setContent(tabContent("PreNotification"))
+
     addStatus("Loaded")
   }
-  
-  private def addStatus(msg: String) = {
+
+  private def addStatus(msg: String) {
     val label = new Label(msg)
     statusBarBox.getChildren.add(label)
   }
 
-  private def tabContent(resourceName: String) : Node = {
+  private def tabContent(resourceName: String): Node = {
     val sp = new scalafx.scene.control.ScrollPane
     sp.fitToHeight = true
     sp.fitToWidth = true
